ce661a584a0e96918632bc41d6071fba8ca5d3f0
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2024-10-14 18:16:02 +0530

    loops commit

---
+++ javascript/loopsinJS/script.js
@@ -0,0 +1,24 @@
+// will be discussing about loops in this lecture...
+// loop is a block of code that executes required number of times specified by the user....
+// In javascript, there are 6 types of loops...
+
+// 1) for loop (general/regular for loop that runs for a specified number of times...)
+//    for loop is preferred when you know exactly how many iterations to be performed beforehand...
+
+
+    // Syntax of for loop...
+
+    // for(initialization;condition;increment/decrement)
+    
+/*      {
+            // lines of code...
+        }
+
+*/
+
+// A simple for loop printing from 1 to 10...
+
+for(i=1;i<=10;i++){
+    console.log(i);
+}
+
