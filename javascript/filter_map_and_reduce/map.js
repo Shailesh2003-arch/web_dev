14bc740731840a548054825fd2345fdeb0d8ecc6
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2024-10-18 21:02:37 +0530

    update

--- javascript/filter_map_and_reduce/map.js
+++ javascript/filter_map_and_reduce/map.js
@@ -1,18 +1,31 @@
 // map() method  in JavaScript is used to create a new array by calling a provided function on every element in the original array....
 // It applies the function to each element and returns the result in a new array without modifying the original array. 
+// mostly useful in shopping cart to calculate price of the items added into the cart and show the total...
+
+// const arr1 = [1,2,3,4,5,6,7,8,9,10];
+// let reciever = arr1.map((num)=>{
+//     return num*10;
+// })
+
+// console.log(reciever);
 
 
-const arr1 = [1,2,3,4,5,6,7,8,9,10];
-let reciever = arr1.map((num)=>{
-    return num*10;
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+let newNums = myNums.map((element)=>{
+    return element*2;
 })
 
-console.log(reciever);
+// console.log(newNums);
 
+// const inital_val = 0;
 
+// inital_value + current_val...
 
-let event_ele = arr1.map((element)=>{
-    return element%2==0;
-})
+const sum = myNums.reduce((inital_value,curr_value)=>{
+    console.log(`inital value is : ${inital_value}`);
+    console.log(`current value is : ${curr_value}`);
 
-console.log(event_ele);
+    return inital_value + curr_value;
+},0)
+
+console.log(sum);
